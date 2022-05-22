959d1ea6e0480ffb270ca0542abb673b813483ed
tangyw <tangyw@landray.com.cn>
2022-05-22 17:13:48 +0800

    feat:Refactor project

---
+++ centos-7-vnc/scripts/apps/idea.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# tomcat
+cp /mnt/c/share/同步空间/remote-dev/ideaIU-2022.1.1.tar.gz /app
+
+cd /app
+tar -zxvf ideaIU-2022.1.1.tar.gz
+rm ideaIU-2022.1.1.tar.gz
+
+echo "idea installed success."
+
+
+
