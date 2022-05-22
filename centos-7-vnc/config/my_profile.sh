726359548a24b36a87d21425411863a8a5101ed6
terwer <youweics@163.com>
2022-05-23 04:11:26 +0800

    fix:Fix input

--- centos-7-vnc/config/my_profile.sh
+++ centos-7-vnc/config/my_profile.sh
@@ -6,4 +6,14 @@ alias la='ls -a'
 alias lla='ls -l -a'
 # echo "alias setup finished"
 
+# 输入法
+export LC_CTYPE=zh_CN.UTF-8
+export XIM=fcitx
+export XIM_PROGRAM=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+# eval `dbus-launch --sh-syntax --exit-with-session`
+# exec fcitx &
+
 source /config/my_profile_auto.sh
